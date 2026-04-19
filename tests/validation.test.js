cdb4c94e8e822fc0fe9b1e099163130fcff0c086
tyra3 <repingastyra@gmail.com>
2026-04-19 18:21:44 +0200

    Changes

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -1,31 +1,31 @@
 const { validateInput, validateOutput } = require('../src/calculator/validation');
 
 //Testing the input values.
-test('valid hex input passes', () => {
+test('inputs with valid hexadecimal format passes', () => {
   expect(validateInput("1A")).toBe(true);
 });
 
 
-test('single digit input should also pass', () => {
+test('input digits with single values pass', () => {
   expect(validateInput("F")).toBe(true);
 }); //Up to two hexadecimal input characters should be valid. 
 
 
-test('rejects non-hex characters', () => {
+test('input values with non-hexadecimal characters are rejected', () => {
   expect(validateInput("1G")).toBe(false);
   expect(validateInput("ZZ")).toBe(false);
 }); //This test checks to ensure that non-hexadecimal character will get rejected
 
-test('input longer than 2 digits gets rejected', () => {
+test('input values with more than 2 digits are rejected', () => {
   expect(validateInput("ABC")).toBe(false);
 });// FR 1.3  and 1.4 states that the system should reject any hexadecimal digits that are longer than 2 digits.
 //This test checks to ensure that the hexadecimal only have up to 2 digits. Anything beyond that comes back as false.
 
-test('empty input should fail', () => {
+test('empty inputs fail', () => {
   expect(validateInput("")).toBe(false);
 }); //Empty inputs will not be accepted.
 
-test('null and undefined should not break anything', () => {
+test('null and undefined values should not break anything', () => {
   expect(validateInput(null)).toBe(false);
   expect(validateInput(undefined)).toBe(false);
 });
@@ -33,16 +33,16 @@ test('null and undefined should not break anything', () => {
 
 
 // Testing the output values.
-test('valid 4 digit hex output passes', () => {
+test('valid 4 digit hexadecimal output passes', () => {
   expect(validateOutput("1A2F")).toBe(true);
 }); //A valid output should have a maximum of 4 digits
 
-test('output longer than 4 digits fails', () => {
+test('output longer with longer than 4 digits fails', () => {
   expect(validateOutput("ABCDE")).toBe(false);
 }); //An output with more than 4 digits will be failed.
 
 
-test('negative output not allowed', () => {
+test('negative numbers are not allowed as outputs', () => {
   expect(validateOutput("-1A")).toBe(false);
 }); //Output values with negative numbers will not pass.
 
