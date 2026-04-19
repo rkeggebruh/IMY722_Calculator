11e489df3c6710deae441be9d40d98ea8f8cc614
rkeggebruh <u20426586@tuks.co.za>
2026-04-19 10:27:16 +0200

    Basic add, multiply, subtract, divide functions

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,5 +1,25 @@
 function add(a, b) {
-  return (parseInt(a, 16) + parseInt(b, 16)).toString(16).toUpperCase();
+  return (parseInt(a, 16) + parseInt(b, 16))
+    .toString(16)
+    .toUpperCase();
 }
 
-module.exports = { add };
+function subtract(a, b) {
+  return (parseInt(a, 16) - parseInt(b, 16))
+    .toString(16)
+    .toUpperCase();
+}
+
+function multiply(a, b) {
+  return (parseInt(a, 16) * parseInt(b, 16))
+    .toString(16)
+    .toUpperCase();
+}
+
+function divide(a, b) {
+  return Math.floor(parseInt(a, 16) / parseInt(b, 16))
+    .toString(16)
+    .toUpperCase();
+}
+
+module.exports = { add, subtract, multiply, divide };
